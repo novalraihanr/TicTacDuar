326af300c69730a2951d7c52bdca658a348b718d
Noval Raihan Ramadhan <Sleepyneko23@gmail.com>
2023-11-24 16:09:04 +0700

    Changes

--- TicTacDuar/app/src/main/java/com/example/tictacduar/ScoreDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.example.tictacduar;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {Score.class},version = 1)
-public abstract class ScoreDatabase extends RoomDatabase {
-    public abstract ScoreDAO getScoreDAO();
-}
